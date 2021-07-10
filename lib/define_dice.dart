86f75de580bf5fb46576d27b174dac731aceddff
acannie <acannie0530@gmail.com>
2021-07-10 19:42:48 +0900

    code: 面のインデックス表示を実装。

--- lib/define_dice.dart
+++ lib/define_dice.dart
@@ -20,47 +20,48 @@ class DefineDice extends StatelessWidget {
             nkoController.initDice();
           },
         ),
-        Container(
-          height: 50,
-          margin: const EdgeInsets.only(top: 10.0),
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: nkoController.dices.length,
-            itemBuilder: (context, index) {
-              textFieldControllers
-                  .add(TextEditingController(text: nkoController.dices[index]));
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: nkoController.dices.length,
+          itemBuilder: (context, index) {
+            textFieldControllers
+                .add(TextEditingController(text: nkoController.dices[index]));
 
-              return SizedBox(
-                width: 125,
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.red),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextField(
-                    inputFormatters: [
-                      LengthLimitingTextInputFormatter(1),
-                    ],
-                    decoration: InputDecoration(
-                      suffix: InkWell(
-                        child: const Icon(Icons.close),
-                        onTap: () {
-                          if (nkoController.dices.isNotEmpty) {
-                            nkoController.removeDiceAt(index);
-                          }
-                        },
-                      ),
-                      hintText: "（例）イ",
+            return Row(
+              children: [
+                Text((index + 1).toString() + "面"),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red),
+                      borderRadius: BorderRadius.circular(5),
                     ),
-                    controller: textFieldControllers[index],
-                    onChanged: (text) {
-                      nkoController.dices[index] = text;
-                    },
+                    child: TextField(
+                      inputFormatters: [
+                        LengthLimitingTextInputFormatter(1),
+                      ],
+                      decoration: InputDecoration(
+                        suffix: InkWell(
+                          child: const Icon(Icons.close, size: 20),
+                          onTap: () {
+                            if (nkoController.dices.isNotEmpty) {
+                              nkoController.removeDiceAt(index);
+                            }
+                          },
+                        ),
+                        hintText: "（例）イ",
+                      ),
+                      controller: textFieldControllers[index],
+                      onChanged: (text) {
+                        nkoController.dices[index] = text;
+                      },
+                    ),
                   ),
                 ),
-              );
-            },
-          ),
+              ],
+            );
+          },
         ),
         Row(
           children: [
