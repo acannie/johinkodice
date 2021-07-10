8624ff20c5fe8bb5a020d61228e8034b319bf3e0
acannie <acannie0530@gmail.com>
2021-07-11 00:20:51 +0900

    code: タイトルが左寄せになるよう修正。

--- lib/layout.dart
+++ lib/layout.dart
@@ -7,24 +7,27 @@ class JohinkodiceLayout {
   static Widget titleStyle(String text, String imagePath) {
     return Container(
       padding: const EdgeInsets.only(top: 50, bottom: 10),
-      child: Wrap(
-        children: <Widget>[
-          Container(
-            height: 50.0,
-            child: Image(image: AssetImage(imagePath)),
-          ),
-          const Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
-          Text(
-            text,
-            overflow: TextOverflow.clip,
-            textAlign: TextAlign.start,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 40,
-              color: Colors.indigo[900],
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Wrap(
+          children: <Widget>[
+            SizedBox(
+              height: 50.0,
+              child: Image(image: AssetImage(imagePath)),
             ),
-          ),
-        ],
+            const Padding(padding: EdgeInsets.symmetric(horizontal: 10)),
+            Text(
+              text,
+              overflow: TextOverflow.clip,
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                color: Colors.indigo[900],
+              ),
+            ),
+          ],
+        ),
       ),
       // child: Text(
       //   text,
