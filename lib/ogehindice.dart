81959e97b2d5a055edb585960f9919d7a6fa6cf9
acannie <acannie0530@gmail.com>
2021-07-17 20:16:31 +0900

    code: 5000兆円のフォントを実装。

--- lib/ogehindice.dart
+++ lib/ogehindice.dart
@@ -1,7 +1,11 @@
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'controller.dart';
+
+GlobalKey globalKey = GlobalKey();
 
 // キーワードを定義するテキストフィールドのリスト
 class PlayJohindice extends StatelessWidget {
@@ -11,8 +15,185 @@ class PlayJohindice extends StatelessWidget {
         Provider.of<NkoCharController>(context);
     List<TextEditingController> textFieldControllers = [];
 
+    // グラデーション
+    Gradient redGradient = const LinearGradient(
+      colors: [
+        Color.fromARGB(255, 241, 61, 47),
+        Color.fromARGB(255, 95, 16, 19),
+      ],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+    );
+
+    Gradient goldGradient = const LinearGradient(
+      colors: [
+        Colors.white,
+        Color.fromARGB(255, 236, 236, 186),
+        Color.fromARGB(255, 181, 109, 33),
+        Color.fromARGB(255, 49, 32, 14),
+        Color.fromARGB(255, 181, 109, 33),
+        // Color.fromARGB(255, 236, 236, 186),
+        Colors.white,
+      ],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+    );
+
+    Gradient whiteGradient = const LinearGradient(
+      colors: [
+        Colors.white,
+        Colors.white,
+        Colors.grey,
+        Colors.white,
+        Colors.white,
+      ],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+    );
+
+    // 5000兆円のフォント
+    Stack premiumText(String text) {
+      return Stack(
+        children: <Widget>[
+          // 影3
+          Text(
+            text,
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              fontSize: 42.5,
+              foreground: Paint()
+                ..style = PaintingStyle.stroke
+                ..strokeWidth = 13
+                ..color = Colors.black,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+
+          // 影2
+          ShaderMask(
+            shaderCallback: (bounds) => whiteGradient.createShader(
+              Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+            ),
+            child: Text(
+              text,
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                fontSize: 42,
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 13
+                  ..color = Colors.white,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+
+          // 影1
+          Text(
+            text,
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              fontSize: 41,
+              foreground: Paint()
+                ..style = PaintingStyle.stroke
+                ..strokeWidth = 13
+                ..color = Colors.black,
+              fontWeight: FontWeight.w900,
+              shadows: const <Shadow>[
+                Shadow(
+                  color: Colors.black,
+                  offset: Offset(-3, 0),
+                  blurRadius: 0.0,
+                ),
+              ],
+            ),
+          ),
+
+          // 金枠
+          ShaderMask(
+            shaderCallback: (bounds) => goldGradient.createShader(
+              Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+            ),
+            child: Text(
+              text,
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                fontSize: 40,
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 12
+                  ..color = Colors.white,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+
+          // 白枠
+          ShaderMask(
+            shaderCallback: (bounds) => whiteGradient.createShader(
+              Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+            ),
+            child: Text(
+              text,
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                fontSize: 40,
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 4
+                  ..color = Colors.white,
+                fontWeight: FontWeight.w900,
+                shadows: <Shadow>[
+                  Shadow(
+                    color: Colors.black87,
+                    offset: Offset(3.0, 3.0),
+                    blurRadius: 0.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // 内側の文字の光
+          Text(
+            text,
+            style: const TextStyle(
+              fontStyle: FontStyle.italic,
+              fontSize: 40,
+              color: Colors.white,
+              fontWeight: FontWeight.w900,
+              shadows: <Shadow>[
+                Shadow(
+                  color: Color.fromARGB(255, 202, 127, 124),
+                  offset: Offset(-1.0, -1.0),
+                  blurRadius: 0.0,
+                ),
+              ],
+            ),
+          ),
+
+          // 内側の文字
+          ShaderMask(
+            shaderCallback: (bounds) => redGradient.createShader(
+              Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+            ),
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontStyle: FontStyle.italic,
+                fontSize: 40,
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
     return Column(
       children: <Widget>[
+        premiumText("5000兆円  "),
         // Play! ボタン
         SizedBox(
           height: 60,
@@ -32,6 +213,7 @@ class PlayJohindice extends StatelessWidget {
             },
           ),
         ),
+        const SizedBox(height: 20),
         // 出た目
         Wrap(
           children: <Widget>[
@@ -52,20 +234,26 @@ class PlayJohindice extends StatelessWidget {
                       style: const TextStyle(fontSize: 30),
                     ),
                   ),
-                  SizedBox(width: 10),
+                  const SizedBox(width: 10),
                 ],
               ),
           ],
         ),
+        const SizedBox(height: 20),
         // 完成した単語
         ListView.builder(
           shrinkWrap: true,
           itemCount: nkoController.results.length,
           itemBuilder: (BuildContext context, int index) {
             return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Expanded(
-                  child: Text(nkoController.results[index]),
+                  child: Text(
+                    nkoController.results[index],
+                    style: TextStyle(color: Colors.red[800], fontSize: 50),
+                  ),
                 ),
               ],
             );
